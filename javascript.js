b2f8269c81f00db94ab439bdee4ed81825039162
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-02-12 17:29:43 -0700

    first commit from tutor sesh

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let hi = "2+2";
+// console.log(hi);
+ let fun = ["car", "truck", "tree"];
+var whatever = 1;
+console.log(fun [whatever]);
+whatever ++
+ console.log(fun [whatever]);
+ console.log(fun [1]);
+for (let num = 0; num < 3 ; num += 1) {
+console.log(num);
+console.log(fun [num]);
+
+    };
+console.log("hi");
+
