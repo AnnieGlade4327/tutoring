5fc84ee49ad68e3fd349362e25657223da914f2a
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-02-13 20:10:14 -0700

    test

--- javascript.js
+++ javascript.js
@@ -1,20 +1,29 @@
 let hi = "hi";
 // console.log(hi);
- let fun = ["one", "two", "three"];
+let fun = ["one", "two", "three"];
 var whatever = 1;
-console.log(fun [whatever + 1]);
-console.log(fun [1]);
-console.log(fun [whatever -1]);
-whatever ++
- console.log(fun [whatever]);
- console.log(fun [1]);
+console.log(fun[whatever + 1]);
+console.log(fun[1]);
+console.log(fun[whatever - 1]);
+whatever++;
+console.log(fun[whatever]);
+console.log(fun[1]);
 
-for (let num = 0; num >= 10 ; num + 1) {
-console.log(num);
-console.log(fun [num]);
-    };
+for (let num = 0; num >= 10; num += 1) {
+  console.log(num);
+  console.log(fun[num]);
+}
 console.log("fun");
 
 let doopdeeDoo = "duped";
 console.log(typeof doopdeeDoo.toUpperCase);
 console.log(doopdeeDoo.toUpperCase());
+function getFun() {
+    let length= parseInt(
+        prompt("how many?")
+    );
+    if (isNaN(length) === true) {
+        alert("whaaaaaaa");
+        return;
+    }
+}
