e6f819598bedbe975d51b7007dd1ace047dcceef
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-02-13 19:25:48 -0700

    at this point, when I tried to console log fun -2, agt line 5 i got a NaN

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
 let hi = "hi";
 // console.log(hi);
  let fun = ["one", "two", "three"];
-var whatever = 2;
+var whatever = 1;
+console.log(fun -2);
 console.log(fun [whatever]);
-whatever --
+whatever ++
  console.log(fun [whatever]);
- console.log(fun [2]);
-for (let num = 0; num <= 99 ; num + 1) {
+ console.log(fun [1]);
+
+for (let num = -10; num >= 55 ; num += 1) {
 console.log(num);
 console.log(fun [num]);
 
     };
-console.log("hi");
+console.log("fun");
 
 let doopdeeDoo = "duped";
 console.log(typeof doopdeeDoo.toUpperCase);
